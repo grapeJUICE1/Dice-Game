5a5c5e3ee08137a6e1f29d4464c56c1dc155354d
Abrar Ahnaf <66725692+grapeJUICE1@users.noreply.github.com>
2021-05-09 20:10:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,17 @@
-var randomNumber1 = Math.round((Math.random() * 6 + 1));
+const dice1 = Math.round((Math.random() * 6 + 1));
 
+document.querySelector(`.img${dice1}`).setAttribute("src", `images/dice${dice1}.png`);
 
+const dice2 = Math.round((Math.random() * 6 + 1));
 
-if (randomNumber1 === 1) {
-    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-};
+document.querySelector(`.img${dice2}`).setAttribute("src", `images/dice${dice2}.png`);
 
-if (randomNumber1 === 2) {
-    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-};
-if
- (randomNumber1 === 3) {
-    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-};
+let heading = ""
 
-if (randomNumber1 === 4) {
-    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-};
+if(dice1 === dice2){
+    heading = "Draw!!"
+}else{
+    heading =  `Player ${Math.max(dice1,dice2)} wins! &nbsp &nbsp<i class='far fa-flag icon'></i>`
+}
 
-if (randomNumber1 === 5) {
-    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-};
-
-if (randomNumber1 === 6) { 
-    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-};
-
-
-randomNumber1;
-
-
-var randomNumber2 = Math.round((Math.random() * 6 + 1));
-
-
-if (randomNumber2 === 1) {
-    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-};
-
-if (randomNumber2 === 2) {
-    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-};
-
-if (randomNumber2 === 3) {
-    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-};
-
-if (randomNumber2 === 4) {
-    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-};
-
-if (randomNumber2 === 5) {
-    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-};
-
-if (randomNumber2 === 6) {
-    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-};
-
-randomNumber2;
-
-if (randomNumber1 > randomNumber2) {
-    document.querySelector(".heading").innerHTML = "<h1><i class='far fa-flag icon'> &nbsp &nbsp</i> Player 1 wins!</h1>"
-};
-
-if(randomNumber1 < randomNumber2){
-    document.querySelector(".heading").innerHTML = "<h1> Player 2 wins! &nbsp &nbsp<i class='far fa-flag icon'></i> </h1>"
-};
-if(randomNumber1 === randomNumber2){
-    document.querySelector(".heading").innerHTML = "<h1>Draw!!</h1>"
-};
+document.querySelector(".heading").innerHTML = `<h1>${heading}</h1>`
