15159f2096475eaae8d3091d5771e53b4fbbb275
Abrar Ahnaf <66725692+grapeJUICE1@users.noreply.github.com>
2021-05-09 21:19:15 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const dice1 = Math.round((Math.random() * 6 + 1));
 
-document.querySelector(`.img${dice1}`).setAttribute("src", `images/dice${dice1}.png`);
+
+document.querySelector(".img1").setAttribute("src", `images/dice${dice1}.png`);
 
 const dice2 = Math.round((Math.random() * 6 + 1));
 
-document.querySelector(`.img${dice2}`).setAttribute("src", `images/dice${dice2}.png`);
+document.querySelector(".img2").setAttribute("src", `images/dice${dice2}.png`);
 
 let heading = ""
 
